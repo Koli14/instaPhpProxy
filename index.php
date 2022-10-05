19ebaa2a81657b48d0dd1384bf915010392ba6d4
Koli14 <koli14@gmail.com>
2022-10-05 11:06:31 +0200

    basics

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-$csirke = $_ENV['ALMA'];
-echo($csirke);
+$instaAccessToken = $_ENV['INSTA_ACCES_TOKEN'];
+$instaId = $_ENV['INSTA_USER_ID'];
+$limit = 8;
+// See the list of possible fields here: https://developers.facebook.com/docs/instagram-basic-display-api/reference/media#fields
+$fields = 'id,caption,media_url,permalink';
+$baseUrl = "https://graph.instagram.com/v6.0/{$instaId}/media?fields={$fields}&limit={$limit}&access_token=";
+
+
+$json = file_get_contents($baseUrl+$instaAccessToken);
+$obj = json_decode($json);
+echo $obj;
