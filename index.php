d313202718a7cef0974d874cbe0e1e693d2a84df
Koli14 <koli14@gmail.com>
2022-10-05 13:40:53 +0200

    finalize

--- index.php
+++ index.php
@@ -19,19 +19,22 @@ $fields = 'id,caption,media_url,permalink';
 $baseUrl = "https://graph.instagram.com/v6.0/{$instaId}/media?fields={$fields}&limit={$limit}&access_token=";
 
 $cacheKey = 'instaProxy';
-$ttl = 60 * 15; // 15 minutes.
-
-// Create the logger
-$logger = new Logger($cacheKey);
-// Now add some handlers
-$logger->pushHandler(new StreamHandler(__DIR__.'/'.$cacheKey.'.log', Level::Info));
-$logger->pushHandler(new FirePHPHandler());
 
 // Checks if there is data in the cache by key
 $cachedData = apcu_fetch($cacheKey);
 if(!$cachedData) {
   $cachedData = file_get_contents($baseUrl . $instaAccessToken);
+  $ttl = 60 * 15; // 15 minutes.
   $isStored = apcu_store($cacheKey, $cachedData, $ttl);
+
+  $dateFormat = "Y.m.d H:i:s";
+  $output = "[%datetime%] > %level_name% > %message% %context%\n";
+  $formatter = new LineFormatter($output, $dateFormat);
+  $stream = new StreamHandler(__DIR__.'/'.$cacheKey.'.log', Level::Info);
+  $stream->setFormatter($formatter);
+  $logger = new Logger($cacheKey);
+  $logger->pushHandler($stream);
+  $logger->pushHandler(new FirePHPHandler());
   $logger->info("Cache generated: ", [$isStored]);
 }
 
--- refresh.php
+++ refresh.php
@@ -1,31 +1,15 @@
 <?php
-use Monolog\Level;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $instaAccessToken = $_ENV['INSTA_ACCES_TOKEN'];
-$instaId = $_ENV['INSTA_USER_ID'];
 $baseUrl = 'https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=';
-$instance = 'refreshToken';
 
-$logger = new Logger($instance);
-$logger->pushHandler(new StreamHandler(__DIR__.'/'.$instance.'.log', Level::Info));
-$logger->pushHandler(new FirePHPHandler());
 
 // Use a free Cron Job service (e.g.: https://cron-job.org), and call this endpoint weekly, as Long-Lived Tokens are valid for only 60 days
 // With calling this endpoint, your token will be refreshed to 60 days
 // More info: https://developers.facebook.com/docs/instagram-basic-display-api/guides/long-lived-access-tokens#get-a-long-lived-token
 
 $data = file_get_contents($baseUrl . $instaAccessToken);
-$logger->info("Trying to refresh instaToken DONE.", [$data]);
 echo("Trying to refresh instaToken DONE.");
-var_dump($data);
