a3571897b685a490ac7c350b29fd2eac115e545c
Koli14 <koli14@gmail.com>
2022-10-05 14:09:31 +0200

    Fine tune

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
 use Monolog\Formatter\LineFormatter;
 
-ini_set('display_errors', '1');
+/* ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-
+ */
 require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -17,14 +17,14 @@ $instaId = $_ENV['INSTA_USER_ID'];
 $limit = 8;
 // See the list of possible fields here: https://developers.facebook.com/docs/instagram-basic-display-api/reference/media#fields
 $fields = 'id,caption,media_url,permalink';
-$baseUrl = "https://graph.instagram.com/v6.0/{$instaId}/media?fields={$fields}&limit={$limit}&access_token=";
+$url = "https://graph.instagram.com/v6.0/{$instaId}/media?fields={$fields}&limit={$limit}&access_token={$instaAccessToken}";
 
 $cacheKey = 'instaProxy';
 
 // Checks if there is data in the cache by key
 $cachedData = apcu_fetch($cacheKey);
 if(!$cachedData) {
-  $cachedData = file_get_contents($baseUrl . $instaAccessToken);
+  $cachedData = file_get_contents($url);
   $ttl = 60 * 15; // 15 minutes.
   $isStored = apcu_store($cacheKey, $cachedData, $ttl);
 
@@ -36,12 +36,13 @@ if(!$cachedData) {
   $logger = new Logger($cacheKey);
   $logger->pushHandler($stream);
   $logger->pushHandler(new FirePHPHandler());
-  $logger->info("Cache generated: ", [$isStored]);
+  $logger->info("Cache generated:", [$isStored]);
 }
 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Origin: https://szinhaz.online");
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Vary: Origin");
 echo ($cachedData);
 exit();
